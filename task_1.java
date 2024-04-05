53a847004922b676e8bc0d962a39ba7aca72bf74
Anna Markaryan <anna.mark.6@yandex.ru>
2024-04-05 11:26:03 +0300

    Удаление лишних файлов

--- task_1.java
+++
@@ -1,6 +0,0 @@
-@Test
-public void checkIsAdultWhenAgeIsMoreThan18True() {
-    Program program = new Program();
-    boolean isAdult = program.checkIsAdult(19);
-	assertEquals("Текст ошибки", true, isAdult);// Напиши код здесь
-}
